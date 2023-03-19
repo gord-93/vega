601ff3e4cabf4a34807dab73826ce3496ef3271e
gord-93 <kos1993@icloud.com>
2023-03-19 20:53:05 +0300

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,18 +5,20 @@ function Header() {
   return (
     <header className="header">
       {/* <img className='header__logo' src={logo} alt="логотип гостиницы Вега" /> */}
-      <div className='header__logo-container'>
-        <h1 className='header__title'>Отель Вега</h1>
-        <div className='header__adress-container'>
-          <img className='header__map' src={map} alt="*" />
-          <p className='header__adress'>г.Наро-Фоминск ул.Московская, д.6</p>
+      <div className='header__container'>
+        <div className='header__logo-container'>
+          <h1 className='header__title'>Отель Вега</h1>
+          <div className='header__adress-container'>
+            <img className='header__map' src={map} alt="*" />
+            <p className='header__adress'>г.Наро-Фоминск ул.Московская, д.6</p>
+          </div>
         </div>
-      </div>
-      <div className='header__links'>
-        <a href='#about' className='header__link'>О нас</a>
-        <a href='#rooms' className='header__link'>номерной фонд</a>
-        <a href='#registration' className='header__link'>бронирование</a>
-        <a href='#contacts' className='header__link'>контакты</a>
+        <div className='header__links'>
+          <a href='#about' className='header__link'>О нас</a>
+          <a href='#rooms' className='header__link'>Номерной фонд</a>
+          <a href='#registration' className='header__link'>Бронирование</a>
+          <a href='#contacts' className='header__link'>Контакты</a>
+        </div>
       </div>
     </header>
   )
