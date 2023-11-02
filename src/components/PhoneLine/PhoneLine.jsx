9172406e5d541ebb3ded47fa7dac794c00886ed5
Eugene Gordievsky <eugene.gordievsky@yandex.ru>
2023-11-02 22:59:32 +0300

    reserve button

--- src/components/PhoneLine/PhoneLine.jsx
+++ src/components/PhoneLine/PhoneLine.jsx
@@ -1,4 +1,4 @@
-import calendareIcon from '../../images/calendare.svg';
+{/*import calendareIcon from '../../images/calendare.svg';*/}
 
 function PhoneLine() {
   const iphoneMobile = new RegExp('iPhone');
@@ -10,7 +10,7 @@ function PhoneLine() {
         <a className="phoneline__phone-link" href="tel: +74963446595">8(496)344-65-95</a>
         <a className="phoneline__phone-link" href="tel: +79154284888">8(915)428-48-88</a>
       </div>
-      <button className="phoneline__button">Забронировать <img className="phoneline__button-icon" src={calendareIcon} alt="Иконка календаря" /></button>
+      {/*<button className="phoneline__button">Забронировать <img className="phoneline__button-icon" src={calendareIcon} alt="Иконка календаря" /></button>*/}
     </div>
   )
 }
